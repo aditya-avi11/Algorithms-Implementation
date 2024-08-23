f093fa809277f107821ebbdf2b3e865283b6651a
Aditya <aadi.avi990@gmail.com>
2024-08-23 19:03:36 +0530

    1st

---
+++ Sorting Algorithms/101-Bubble_Sort.c
@@ -0,0 +1,28 @@
+// Concept : Pushes the maximum to the last by adjacent swaps.
+#include<stdio.h>
+int main() {
+    int arr[] = {13,46,24,52,20,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i,j,swap;
+    int flag=0; //will help in indicating if the array is sorted in the first iteration
+
+    for(i=0; i<n; i++) {
+        for(j=0; j<n-i-1; j++) {
+            if(arr[j]>arr[j+1]) {
+                swap = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = swap;
+                flag=1;
+            }
+        }
+        if(flag==0) { printf("Array already sorted!\n"); break; }
+    }
+
+    for(int i=0; i<n; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    return 0;
+}
+
+//Complexity : Worst Case : O(n²), Best Case : O(n) [when already sorted, identified in 1st iteration]
